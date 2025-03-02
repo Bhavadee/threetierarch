ac997a05c0aaca2e1b57943a61b7df26adbf213c
bhavdeep reddy <87595670+Bhavadee@users.noreply.github.com>
2025-03-02 14:10:55 +0530

    for 3tier application cicd

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'myyt3tierdb.cxoewi64c2gn.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'webappdb'
+});
